e5e0c2696db67ef1df66706f6b39612a2831d448
miguepoloc <miguepoloc@gmail.com>
2023-10-22 11:29:56 -0500

    :construction_worker: feat: delete static in build

--- build.sh
+++ build.sh
@@ -9,7 +9,3 @@ python -m pip install --upgrade pip
 # Build the project
 echo "Building the project..."
 python -m pip install -r requirements.txt
-
-# Collect static files
-echo "Collecting static files..."
-python manage.py collectstatic --noinput --clear
